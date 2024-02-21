9c65ca1f4d966605ac605ac4a4c8eb00a58bc31f
BeluKotu <vietthanh456@gmail.com>
2024-02-21 18:10:07 +0700

    Refactor FTextField component

--- client/src/components/form/FTextField.js
+++ client/src/components/form/FTextField.js
@@ -2,23 +2,24 @@ import { useFormContext, Controller } from "react-hook-form";
 import { TextField } from "@mui/material";
 
 function FTextField({ name, ...other }) {
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-                <TextField
-                    {...field}
-                    fullWidth
-                    error={!!error}
-                    helperText={error?.message}
-                    {...other}
-                />)
-            }
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue=""
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          error={!!error}
+          helperText={error?.message}
+          {...other}
         />
-    );
+      )}
+    />
+  );
 }
 
 export default FTextField;
